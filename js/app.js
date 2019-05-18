187763e5c9e236a1823898a525797063eed7ff2b
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-05-18 02:00:52 +0100

    First attempt at creating offline service worker. Working in Firefox not Chrome

---
+++ js/app.js
@@ -0,0 +1,13 @@
+if (navigator.serviceWorker) {
+    console.log("ServiceWorker: support detected.");
+    async function registerServiceWorker() {
+        console.log("ServiceWorker: registering.");
+        try {
+            const reg = await navigator.serviceWorker.register('./js/app_worker.js');
+            console.log("ServiceWorker: registered", reg);
+        } catch (e) {
+            console.error("Service Worker failed.  Falling back to online-only.", e);
+        }
+    }
+    window.addEventListener("load", registerServiceWorker);
+};
